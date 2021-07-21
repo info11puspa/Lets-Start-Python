a79a5a8f2ef9223a71bfa4b4441ae3fed98a02f3
info11puspa <52624654+info11puspa@users.noreply.github.com>
2021-07-21 11:18:40 +0900

    Create Basic operators.py

---
+++ Basic operators.py
@@ -0,0 +1,31 @@
+
+''' we do not import any library.\
+    For complicated tasks such as differentiation, integration, \
+    solving the linear or non-linear equaitons,\
+    we can use (import) third party libraries or make our own coding\
+    environment. We will talk about this later.'''
+
+''' Details can be read at:https://www.tutorialspoint.com/python/python_basic_operators.htm'''
+Follow sanjaya sir for details
+
+a = 2
+b = 3
+#Addition, Subtraction
+c = a +,- b    #spaces between the character does not matter 
+#Multiplication
+e = a * b
+# Power
+f = a**b      # power 2
+# divide
+g = a/b
+h = a/float(b)
+i = float(a)/b
+print 'a = {} && b = {} '.format(a,b)
+print 'a+b gives: ',c
+print 'a-b gives: ',d
+print 'a*b gives: ',e
+print 'a**b gives: ',f
+print 'a/b gives: ',g
+print 'a/float(b) gives: {}, is this different from a/b?'.format(h)
+print '**Be careful when dealing with a division in python 2.7.xxx'
+print 'float(a)/b gives: {}, what about this one?'.format(i)
