83f09838114fade4dc78f52e1a8a519edd97686b
info11puspa <52624654+info11puspa@users.noreply.github.com>
2021-07-21 11:20:44 +0900

    Create Classification of data.py

---
+++ Classification of data.py
@@ -0,0 +1,16 @@
+ # For this, we do not need to import any library  
+''' we do not need to import any library'''
+
+a = 'python' # a string
+b = 2 #integer
+c = 2. #float
+d = 2+1j #complex
+
+''' we do not need to assign any memory for the assigned data, python automatically handles all the jobs.
+However, if you want to have your own memory management then you can do it manually (It would be easy with third party
+ numerical libraries like numpy, Theano, tensor flow etc).'''
+
+print 'a has data type', type(a)
+print 'b has data type', type(b)
+print 'c has data type', type(c)
+print 'd has data type', type(d)
