0deee8bb69599a56f584ceea1ebc58ea55315ea5
info11puspa <52624654+info11puspa@users.noreply.github.com>
2021-07-21 11:19:32 +0900

    Create Basic comparison.py

---
+++ Basic comparison.py
@@ -0,0 +1,21 @@
+'''No third party library\
+   The result will be True if the condition is correct otherwise False'''
+
+''' Details can be read at:https://www.tutorialspoint.com/python/python_basic_operators.htm'''
+
+a = 2
+b = 3
+
+#'==', '>', '>=', '<', '<='means equal, greater than, greater than or equal, less than, and less than or equal respectively.
+c = (a==b)
+d = (a>b)
+e = (a>=b)
+f = (a<b)
+g = (a<=b)
+print 'a = 2 and b = 3'
+print '(a==b): ',c
+print '(a>b): ',d
+print '(a>=b): ',e
+print '(a<b): ',f
+print '(a<=b): ',g
+print 'Please read other operations at https://www.tutorialspoint.com/python/python_basic_operators.htm'
